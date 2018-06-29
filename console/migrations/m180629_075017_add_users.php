375f6a7840172c6b02b4073742b017de07fa8bea
Yuri Doronin <dye491@gmail.com>
2018-06-29 11:02:41 +0200

    create migration add_users

---
+++ console/migrations/m180629_075017_add_users.php
@@ -0,0 +1,70 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m180629_075017_add_users
+ */
+class m180629_075017_add_users extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->batchInsert('user', [
+            'id',
+            'username',
+            'email',
+            'auth_key',
+            'password_hash',
+            'created_at',
+            'updated_at',
+        ],
+            [
+                [
+                    1,
+                    'Маша Петрова',
+                    'mary@example.com',
+                    Yii::$app->getSecurity()->generateRandomString(),
+                    Yii::$app->getSecurity()->generatePasswordHash('123456'),
+                    time(),
+                    time(),
+                ],
+                [
+                    2,
+                    'Петя Иванов',
+                    'pete@example.com',
+                    Yii::$app->getSecurity()->generateRandomString(),
+                    Yii::$app->getSecurity()->generatePasswordHash('123456'),
+                    time(),
+                    time(),
+                ],
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        echo "m180629_075017_add_users cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m180629_075017_add_users cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
