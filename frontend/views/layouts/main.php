43597f889e7b92e2b092634df190418ce781547e
Yuri Doronin <dye491@gmail.com>
2018-06-30 18:01:58 +0200

    modify main menu

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -1,6 +1,7 @@
 <?php
 
 /* @var $this \yii\web\View */
+
 /* @var $content string */
 
 use yii\helpers\Html;
@@ -36,22 +37,46 @@ AppAsset::register($this);
         ],
     ]);
     $menuItems = [
-        ['label' => 'Home', 'url' => ['/site/index']],
-        ['label' => 'About', 'url' => ['/site/about']],
-        ['label' => 'Contact', 'url' => ['/site/contact']],
+//        ['label' => 'Home', 'url' => ['/site/index']],
+//        ['label' => 'About', 'url' => ['/site/about']],
+//        ['label' => 'Contact', 'url' => ['/site/contact']],
+        [
+            'label' => Yii::t('app_message', 'Messages'),
+//            'dropDownOptions' => [
+            'items' => [
+                ['label' => Yii::t('app_message', 'Inbox'), 'url' => ['/message']],
+                ['label' => Yii::t('app_message', 'Sent'), 'url' => ['/message/sent']],
+            ],
+//            ],
+        ],
     ];
     if (Yii::$app->user->isGuest) {
-        $menuItems[] = ['label' => 'Signup', 'url' => ['/site/signup']];
-        $menuItems[] = ['label' => 'Login', 'url' => ['/site/login']];
+        $menuItems[] = ['label' => Yii::t('app_user', 'Signup'), 'url' => ['/site/signup']];
+        $menuItems[] = ['label' => Yii::t('app_user', 'Login'), 'url' => ['/site/login']];
     } else {
-        $menuItems[] = '<li>'
-            . Html::beginForm(['/site/logout'], 'post')
-            . Html::submitButton(
-                'Logout (' . Yii::$app->user->identity->username . ')',
-                ['class' => 'btn btn-link logout']
-            )
-            . Html::endForm()
-            . '</li>';
+        /*        $menuItems[] = '<li>'
+                    . Html::beginForm(['/site/logout'], 'post')
+                    . Html::submitButton(
+                        'Logout (' . Yii::$app->user->identity->username . ')',
+                        ['class' => 'btn btn-link logout']
+                    )
+                    . Html::endForm()
+                    . '</li>';*/
+        $menuItems[] = [
+            'label' => Yii::$app->user->identity->username,
+            'items' => [
+                ['label' => Yii::t('app_user', 'Profile'), 'url' => ['/user/profile']],
+                '<li><hr style="margin: 0"></li>',
+                '<li>'
+                . Html::beginForm(['/site/logout'], 'post')
+                . Html::submitButton(Yii::t('app_user', 'Logout'), [
+                    'class' => 'btn btn-link logout',
+                    'style' => 'text-align:left;color:rgb(51,51,51);text-decoration:none;padding:3px 20px 3px 20px;',
+                ])
+                . Html::endForm()
+                . '</li>',
+            ],
+        ];
     }
     echo Nav::widget([
         'options' => ['class' => 'navbar-nav navbar-right'],
