228f5c2f4eebcce846416453c7c42e30c3b15fc7
Yuri Doronin <dye491@gmail.com>
2018-07-06 14:01:21 +0200

    fix color calculation

--- frontend/helpers/ColorHelper.php
+++ frontend/helpers/ColorHelper.php
@@ -6,7 +6,7 @@ class ColorHelper
 {
     public static function color($id)
     {
-        $angle = 45 * ($id % 16);
+        $angle = 22.5 * ($id % 16);
         return "hsl($angle, 70%, 50%)";
     }
 }
